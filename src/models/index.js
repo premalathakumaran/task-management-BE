1ec0b72114944a3517620fc7661f448796ab2a04
premalathakumaran <premalathakumaran18@gmail.com>
2024-04-20 14:09:26 +0530

    last commit

--- src/models/index.js
+++ src/models/index.js
@@ -1,18 +1,4 @@
 
-
-// import mongoose from 'mongoose'
-// import dotenv from 'dotenv'
-// dotenv.config()
-
-// try {
-//     mongoose.connect(`${process.env.dbUrl}/${process.env.dbName}`)
-//     console.log("mongoose connected ")
-// } catch (error) {
-//     console.log(error)
-// }
-// export default mongoose
-
-
 // models/index.js
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,4 @@
 
-
-// import express from 'express'
-// import userRoutes from './user.js'
-// import taskRoutes from './task.js'
-
-// const router = express.Router()
-
-// router.use('/user',userRoutes)
-// router.use('/task',taskRoutes)
-
-//  export default router
-
-
 // routes/index.js
 import express from 'express';
 import userRoutes from './user.js';
